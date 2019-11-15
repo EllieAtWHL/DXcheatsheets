77b9e07e449303617f0bec9db3add7ab8f084e54
eleanormatthewman <eleanormatthewman@gmail.com>
2019-11-15 16:19:24 +0000

    new date snippet

--- javascript/incrementDate.js
+++ javascript/incrementDate.js
@@ -3,3 +3,12 @@
         let dateFormatToTime = new Date(dateInput);
         return new Date(dateFormatToTime.getTime() + (increment * 86400000));
     }
+
+    incrementMonth(dateInput, increment){
+        let dateCheck = dateInput.getDate();
+        dateInput.setMonth(dateInput.getMonth() + +increment);
+        if (dateInput.getDate() != dateCheck){
+            dateInput.setDate(0);
+        }
+        return dateInput;
+    }
